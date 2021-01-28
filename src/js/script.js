11d725bcd4d5816ada11d582bd48922fa6e03725
Jenny Ryden <jennyryden@protonmail.com>
2021-01-28 20:14:41 +0100

    Mobile menu functionality

---
+++ src/js/script.js
@@ -0,0 +1,8 @@
+function toggleMobile() {
+var x = document.getElementById("myNav");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
